18ddaa108ce4c537cce57a216d6aea4e0de26ead
chhavi choudhary <chhavi.2023@tecorb.co>
2024-01-04 12:26:13 +0530

    first example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React,{useCallback} from 'react';
+import ReactFlow,{useEdgesState,useNodesState,addEdge} from 'reactflow';
+ 
+import 'reactflow/dist/style.css';
+ 
+const initialNodes = [
+  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+  { id: '3', position: { x: 0, y: 250 }, data: { label: '3' } },
+];
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' },
+{ id: 'e2-3', source: '2', target: '3' },];
+ 
+export default function App() {
   return (
-   <p>hiii</p>
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <ReactFlow nodes={initialNodes} edges={initialEdges} />
+    </div>
   );
 }
-
-export default App;
